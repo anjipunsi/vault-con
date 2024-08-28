8452079a9e087de53cec1ecaceb761fafa86ee58
Anjali punsi <108992453+anjipunsi@users.noreply.github.com>
2024-08-28 13:58:28 +0530

    Update fetch_credentials.sh

--- fetch_credentials.sh
+++ fetch_credentials.sh
@@ -3,8 +3,8 @@ export VAULT_ADDR='http://192.168.3.38:8200'
 export VAULT_TOKEN='test'
 
 # Retrieve secrets from Vault
-screener_username=$(vault kv get -field=username secret/data/myapp)
-screener_password=$(vault kv get -field=password secret/data/myapp)
+screener_username=$(vault kv get -field=username secret/myapp)
+screener_password=$(vault kv get -field=password secret/myapp)
 
 # Print retrieved secrets
 echo "Retrieved Screener Username: $screener_username"
